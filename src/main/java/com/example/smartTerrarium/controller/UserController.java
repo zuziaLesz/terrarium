03fb23db38bc46ed40c26156dfcda73391e1121e
zuzia <zuzanna.leszczynska1@gmail.com>
2025-01-19 12:59:02 +0100

    register user

--- src/main/java/com/example/smartTerrarium/controller/UserController.java
+++ src/main/java/com/example/smartTerrarium/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.example.smartTerrarium.controller;
 
+import com.example.smartTerrarium.dto.CreateUserDto;
+import com.example.smartTerrarium.dto.UserLoginData;
 import com.example.smartTerrarium.entity.User;
 import com.example.smartTerrarium.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -24,4 +24,16 @@ public class UserController {
         User user = userService.createDefaultUser();
         return ResponseEntity.ok(user);
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> registerUser(@RequestBody CreateUserDto userDto) {
+        userService.registerUser(userDto);
+        return ResponseEntity.noContent().build();
+    }
+
+//    @GetMapping("/login")
+//    public ResponseEntity<User> login(@RequestBody UserLoginData loginData) {
+//        return ResponseEntity.ok(userService.login(loginData));
+//    }
+
 }
--- src/main/java/com/example/smartTerrarium/service/UserService.java
+++ src/main/java/com/example/smartTerrarium/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.smartTerrarium.service;
 
+import com.example.smartTerrarium.dto.CreateUserDto;
+import com.example.smartTerrarium.dto.UserLoginData;
 import com.example.smartTerrarium.entity.User;
 import com.example.smartTerrarium.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +24,26 @@ public class UserService {
                 .password("zuzia")
                 .build();
         return userRepository.save(user);
+    }
+
+    public void registerUser(CreateUserDto userDto) {
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        userRepository.save(user);
+    }
+
+    public User login(UserLoginData loginData) {
+        User user = getUserByEmail(loginData.getEmail());
+        if(user.getPassword() == loginData.getPassword()) {
+            return user;
+        }
+        else throw new RuntimeException();
+    }
+
+    private User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found with email: " + email));
     }
 }
