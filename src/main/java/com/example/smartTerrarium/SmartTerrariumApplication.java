193f9713e8fb95fdaf8d35925bd39b8e561f0020
Zuzia <zuzanna.leszczynska1@gmail.com>
2025-10-15 23:53:30 +0200

    remove unused imports

--- src/main/java/com/example/smartTerrarium/SmartTerrariumApplication.java
+++ src/main/java/com/example/smartTerrarium/SmartTerrariumApplication.java
@@ -1,12 +1,7 @@
 package com.example.smartTerrarium;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class SmartTerrariumApplication {
