f89db659c088bd16f3ff06ecdea086726e776423
Zuzia <zuzanna.leszczynska1@gmail.com>
2025-10-15 23:52:31 +0200

    the application working, database changes

--- src/main/java/com/example/smartTerrarium/SmartTerrariumApplication.java
+++ src/main/java/com/example/smartTerrarium/SmartTerrariumApplication.java
@@ -8,23 +8,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class SmartTerrariumApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartTerrariumApplication.class, args);
-	}
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		Connector ajpConnector = new Connector("AJP/1.3");
-		ajpConnector.setPort(8080);
-		ajpConnector.setSecure(false);
-		ajpConnector.setAllowTrace(false);
-		ajpConnector.setScheme("http");
-		((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
-		tomcat.addAdditionalTomcatConnectors(ajpConnector);
-		return tomcat;
 	}
 }
